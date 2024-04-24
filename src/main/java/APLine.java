57ee5db275ba732260d59af6ef0b67dcd148e374
breadsticker1 <143027797+breadsticker1@users.noreply.github.com>
2024-04-24 00:07:55 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -6,14 +6,14 @@ public class APLine
   public APLine(int a1, int b1, int c1){
     a = a1;
     b = b1;
-    c =c 1;
+    c = c1;
   }
 
   public double getSlope(){
     return -(double)a/b;
   }
 
-  public boolean inOnLine(int x, int y){
+  public boolean isOnLine(int x, int y){
     return a*x + b*y + c == 0;
   }
 }
